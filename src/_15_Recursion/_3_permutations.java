e2cfabc246cf7b1998330f7f7195d4a31c728d2f
LiFangCoding <fanglihust@gmail.com>
2019-05-21 17:07:20 +0900

    permutations using nextpermutation

--- src/_15_Recursion/_3_permutations.java
+++ src/_15_Recursion/_3_permutations.java
@@ -21,6 +21,13 @@ public class _3_permutations {
         return res;
     }
 
+    /**
+     * Count of recursive calls are:
+     * C(n) = 1 + nC(n - 1) for n >= 1. C(0) = 1
+     * C(n) = n!
+     * do O(n) computation per call outside of recursive calls.
+     * T(n) = (n * n!).
+     */
     private static void directedPermutations(List<Integer> A, int i, List<List<Integer>> res) {
         if (i == A.size()) {
             res.add(new ArrayList<Integer>(A));
@@ -33,6 +40,29 @@ public class _3_permutations {
             //Generate all permutations for A.subList(i + 1, A.size())
             directedPermutations(A, i + 1, res);
             Collections.swap(A, i, j);
+        }
+    }
+
+    /**
+     * Use nextPermutation method.
+     *
+     */
+    static class Solution2 {
+        /**
+         * T = (n * n!)
+         * There are n! permutations and we spend O(n) time to store each one.
+         */
+        public static List<List<Integer>> permutations(List<Integer> A) {
+            List<List<Integer>> res = new ArrayList<>();
+
+            //Generate the first permutation in dictionary order.
+            Collections.sort(A);
+            do {
+                res.add(new ArrayList<>(A));
+                A = NextPermutation.nextPermutation(A);
+            } while (!A.isEmpty());
+
+            return res;
         }
     }
 }
