fc78fbe6a40969ad98853dab2956f8e39cfa1226
LiFangCoding <fanglihust@gmail.com>
2019-05-28 01:31:37 +0900

    start _1

---
+++ src/_10_BinaryTrees/_1_TestBinaryTreeHeightBalanced.java
@@ -0,0 +1,14 @@
+package _10_BinaryTrees;
+
+/**
+ * A binary tree is height-blanced if for each node in the tree,
+ * the difference in the height of its left and right subtrees is at most one.
+ * Write a program that takes as input the root of a binary tree and checks whether the tree is
+ * height balanced.
+ */
+public class _1_TestBinaryTreeHeightBalanced {
+    /**
+     * Hint: Think of a classic binary tree algorithm.
+     */
+
+}
