f7013396cd2080cee16013f9fcaad0384fb719a7
LiFangCoding <fanglihust@gmail.com>
2019-05-29 18:33:39 +0900

    TreeHeightBalanced

--- src/_10_BinaryTrees/_1_TestBinaryTreeHeightBalanced.java
+++ src/_10_BinaryTrees/_1_TestBinaryTreeHeightBalanced.java
@@ -1,5 +1,7 @@
 package _10_BinaryTrees;
 
+import common.TreeNode;
+
 /**
  * A binary tree is height-blanced if for each node in the tree,
  * the difference in the height of its left and right subtrees is at most one.
@@ -7,8 +9,51 @@ package _10_BinaryTrees;
  * height balanced.
  */
 public class _1_TestBinaryTreeHeightBalanced {
+    public static boolean isBalanced(TreeNode tree) {
+        return checkBalanced(tree).balanced;
+    }
+
+    /**
+     * postorder traversal with some calls possibly being eliminated
+     * because of early termination.
+     * If any left subtree is not height-balanced, we do not need to visit
+     * the corresponding right subtree.
+     * Space = O(h)
+     * T = O(n). number of nodes.
+     */
+    private static BalancedStatusWithHeight checkBalanced(TreeNode tree) {
+        if (tree == null) {
+            return new BalancedStatusWithHeight(true, -1);
+        }
+
+        BalancedStatusWithHeight left = checkBalanced(tree.left);
+        if (!left.balanced) {
+            // left subtree is not balanced.
+            return left;
+        }
+
+        BalancedStatusWithHeight right = checkBalanced(tree.right);
+        if (!right.balanced) {
+            // right subtree is not balanced.
+            return right;
+        }
+
+        boolean isBalanced = Math.abs(left.height - right.height) <= 1;
+        int height = Math.max(left.height, right.height) + 1;
+        return new BalancedStatusWithHeight(isBalanced, height);
+    }
+
     /**
      * Hint: Think of a classic binary tree algorithm.
      */
 
+    private static class BalancedStatusWithHeight {
+        public boolean balanced;
+        public int height;
+
+        public BalancedStatusWithHeight(boolean balanced, int height) {
+            this.balanced = balanced;
+            this.height = height;
+        }
+    }
 }
