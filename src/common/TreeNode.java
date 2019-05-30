ef6b968c0ceece609ccf5cfc923e25f76021b822
LiFangCoding <fanglihust@gmail.com>
2019-05-30 11:15:31 +0900

    complete LCAWithParent

--- src/common/TreeNode.java
+++ src/common/TreeNode.java
@@ -4,6 +4,7 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode parent;
 
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
