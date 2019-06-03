8935d2662538972b84767b69e3fc627902c91621
LiFangCoding <fanglihust@gmail.com>
2019-06-03 20:55:01 +0900

    Start stack and heaps

---
+++ src/_11_heaps/_1_MergeSortedFiles.java
@@ -0,0 +1,4 @@
+package _11_heaps;
+
+public class _1_MergeSortedFiles {
+}
---
+++ src/_9_StacksAndQueues/_1_ImplementAStackWithMaxAPI.java
@@ -0,0 +1,13 @@
+package _9_StacksAndQueues;
+
+/**
+ * Design a stack that includes a max operation, in addition to push and pop.
+ * The max method should return the maximum value stored in the stack.
+ */
+public class _1_ImplementAStackWithMaxAPI {
+    // Hint: Use additional storage to track the maximum value.
+    // TODO
+    /**
+     *
+     */
+}
