a5ed6a2142b5808762da218f665e7dccc3c8dd59
LiFangCoding <fanglihust@gmail.com>
2019-06-11 10:45:55 +0900

    starting 19_9

---
+++ src/_19_Graphs/_9_ComputeShortestPathFewestEdges.java
@@ -0,0 +1,10 @@
+package _19_Graphs;
+
+/**
+ * Design an algorithm which takes as input a graph G = (V , E), directed or undirected, a nonnegative cost function on E, and vertices s and t;
+ * your algorithm should output a path with the fewest edges amongst all shortest paths from s to t.
+ */
+public class _9_ComputeShortestPathFewestEdges {
+    // Hint: Change the edge cost and cast it as an instance of the standard shortest path problem.
+
+}
