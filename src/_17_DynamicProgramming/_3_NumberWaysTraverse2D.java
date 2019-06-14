ce8385528c2407cc65d741ada2e5229ffe504402
LiFangCoding <fanglihust@gmail.com>
2019-06-14 19:09:29 +0900

    start 17_3

---
+++ src/_17_DynamicProgramming/_3_NumberWaysTraverse2D.java
@@ -0,0 +1,9 @@
+package _17_DynamicProgramming;
+
+/**
+ * Write a program that counts how many ways you can go from the top-left to the bottom-right in a 2D array.
+ */
+public class _3_NumberWaysTraverse2D {
+    // Hint:Ifi>0and j>0,you can get to(i,j)from(i-1,j)or(j-1,i).
+
+}
